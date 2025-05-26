9bfda5c7e28e9f28b9d1f3e8e402bc6c6d872711
Ayiera-20 <ayieramyra@gmail.com>
2025-05-26 18:47:19 +0300

    feat: add home and about page links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,24 @@
 
-
+import { Link } from 'react-router-dom';
 import DrawerMenu from "./drawer";
 
 export default function NavBar() {
  
     return (
-        <div className="md:px-0 px-12 py-4">
+        <div className="md:px-0 px-12 py-4 hover:bg-[#546a45]">
              <div className="flex justify-between items-center lg:px-20">
             <div className="flex items-center gap-x-6">
                <DrawerMenu />
             </div>
-            <div >
+            <nav>
                 <ul className="flex-row justify-between hidden lg:flex gap-14 text-[20px] text-white font-medium">
-                    <li>Home</li>
-                    <li>About</li>
+                    <Link to="/" className="">Home</Link>
+                     <Link to="/about" className="">About</Link>
                     <li>What we do</li>
                     <li>Projects</li>
                     <li>Get Involved</li>
                 </ul>
-            </div>
+            </nav>
             <div className="gap-x-4  hidden lg:flex ">
            
               <button className="py-4 px-8 rounded-full border-2 border-[#afdfaa] text-white hover:bg-[#afdfaa] hover:text-black">Donate Now</button>
