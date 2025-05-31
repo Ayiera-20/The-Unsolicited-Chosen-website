effee2da06b0164351f7a96a9fe2ab9dd16a1fb7
Ayiera-20 <ayieramyra@gmail.com>
2025-05-31 20:55:38 +0300

    add team memebers description

--- src/routes/About/Team.tsx
+++ src/routes/About/Team.tsx
@@ -9,35 +9,35 @@ import profile from "../../assets/images/woman-floral-shirt-smiling.jpg"
 const teamMembers = [
 
     {image:profile, role: "Founder", name: "Jenny Otinda", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        description: "Team volunteering not only allows for greater impact but also fosters camaraderie and teamwork among participants.",
         socialLinks:{
         twitter: "https://twitter.com/devbob",
         instagram: "https://instagram.com/devbob",
         linkedin: "https://linkedin.com/in/bob"
     } },
     {image:profile,role: "Software Developer", name: "Miriam Ayiera", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        description: "Team volunteering not only allows for greater impact but also fosters camaraderie and teamwork among participants.",
         socialLinks:{
         twitter: "https://twitter.com/devbob",
         instagram: "https://instagram.com/devbob",
         linkedin: "https://linkedin.com/in/bob"
     } },
      {image:profile, role: "PR", name: "Hope", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        description: "Team volunteering not only allows for greater impact but also fosters camaraderie and teamwork among participants.",
         socialLinks:{
         twitter: "https://twitter.com/devbob",
         instagram: "https://instagram.com/devbob",
         linkedin: "https://linkedin.com/in/bob"
     } },
      {image:profile, role: "Finance", name: "John", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        description: "Team volunteering not only allows for greater impact but also fosters camaraderie and teamwork among participants.",
         socialLinks:{
         twitter: "https://twitter.com/devbob",
         instagram: "https://instagram.com/devbob",
         linkedin: "https://linkedin.com/in/bob"
     } },
      {image:profile, role: "Graphic Designer", name: "Jackline", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        description: "Team volunteering not only allows for greater impact but also fosters camaraderie and teamwork among participants.",
         socialLinks:{
         twitter: "https://twitter.com/devbob",
         instagram: "https://instagram.com/devbob",
@@ -50,14 +50,14 @@ const Team = () => {
         <div className="px-20 py-30 flex flex-col items-center justify-center">
             <h1 className="text-[64px] mb-20 ">Meet Our Team</h1>
             <div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-15">
                     {teamMembers.map((member, index) => (
                         <div key={index} className="flex flex-col md:flex-row gap-6">
-                            <img className="w-50 h-auto" src ={member.image} alt={member.name} />
+                            <img className="w-45 h-auto" src ={member.image} alt={member.name} />
                             <div className="space-y-4">
                                 <h3 className="text-[#AFDFAA] lg:text-lg">{member.role}</h3>
                                 <p className="text-2xl">{member.name}</p>
-                                <div className="w-60 h-[1px] bg-[#AFDFAA]"></div>
+                                <div className="w-96 h-[1px] bg-[#AFDFAA]"></div>
                                 <p className="text-[18px]">{member.description}</p>
                                 <div className="flex  gap-4 mt-8">
                                     {member.socialLinks.twitter && (
