dcc453fdd4b8a42f846ac2b1770c8709f56bd286
Ayiera-20 <ayieramyra@gmail.com>
2025-05-31 20:59:10 +0300

    feat(navigation): add routes for "What We Do" and "Get Involved" pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './routes/Home/Home'
 import About from './routes/About/About'
+import WhatWeDo from './routes/What_We_Do/What_We_Do'
+import GetInvolved from './routes/Get_Involved/Get_Involved'
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="whatwedo" element={<WhatWeDo />} />
+        <Route path="getinvolved" element={<GetInvolved />} />
       </Routes>
     </BrowserRouter>
   )
