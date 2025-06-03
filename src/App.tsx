9333dd49ce5b5ca93c93e522cd179998fd27cf7d
Ayiera-20 <ayieramyra@gmail.com>
2025-06-03 22:03:23 +0300

    config: add Contact Us page route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './routes/Home/Home'
 import About from './routes/About/About'
 import WhatWeDo from './routes/What_We_Do/What_We_Do'
 import GetInvolved from './routes/Get_Involved/Get_Involved'
+import ContactUs from './routes/Contact/ContactUs'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="whatwedo" element={<WhatWeDo />} />
         <Route path="getinvolved" element={<GetInvolved />} />
+        <Route path="Contact" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,8 +12,8 @@ export default function NavBar() {
             </div>
             <nav>
                 <ul className="flex-row justify-between hidden lg:flex gap-14 text-[20px] text-white font-medium">
-                    <Link to="/" className="">Home</Link>
-                     <Link to="/about" className="">About</Link>
+                    <Link to="/" className=" hover:text-black hover:border-[#afdfaa] hover:bg-[#afdfaa] hover:border-2 active:bg-[#afdfaa] hover:px-3.5 hover:py-1 hover:rounded-lg">Home</Link>
+                     <Link to="/about" className="hover:text-black hover:border-[#799434] hover:bg-[#afdfaa] hover:border-1 active:bg-[#afdfaa] hover:px-3.5 hover:py-1 hover:rounded-lg">About</Link>
                      <Link to="/whatwedo" className="">What we do</Link>
                      <Link to="/getinvolved" className="">Get Involved</Link>
                     <li>Projects</li>
@@ -23,7 +23,10 @@ export default function NavBar() {
             <div className="gap-x-4  hidden lg:flex ">
            
               <button className="py-4 px-8 rounded-full border-2 border-[#afdfaa] text-white hover:bg-[#afdfaa] hover:text-black">Donate Now</button>
-              <button className="py-4 px-8 rounded-full bg-[#afdfaa] text-black border-transparent hover:bg-transparent hover:text-white hover:border-2 hover:border-[#afdfaa]" >Contact Us</button>
+              <Link to="/contact">
+               <button className="py-4 px-8 rounded-full bg-[#afdfaa] text-black border-transparent hover:bg-transparent hover:text-white hover:border-2 hover:border-[#afdfaa]" >Contact Us</button>
+              </Link>
+             
             </div>
             
           </div>
