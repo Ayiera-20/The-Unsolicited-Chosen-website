cc1aa47e46363f58072f952be291d8bb2c97e2fb
Ayiera-20 <ayieramyra@gmail.com>
2025-06-06 00:55:05 +0300

    feat: add hover interactivity to What we do page

--- src/routes/What_We_Do/What_We_Do.tsx
+++ src/routes/What_We_Do/What_We_Do.tsx
@@ -5,40 +5,50 @@ import waste2 from "../../assets/images/freepik__the-style-is-candid-image-photo
 import NavBar from "../../components/navbar";
 
 const items = [
-    {Image: waste, text:"Affordable Waste Solutions", link:""},
-    {Image: waste2, text:"eco-friendly commerce", link:""},
-    {Image:  waste, text:"Community Cleanups", link:""},
-    {Image: waste2, text:"Community Education", link:""},
-    {Image: partnership, text:"Strategic Partnerships", link:""},
-    {Image: partnership, text:"Spreading Faith & Hope", link:""},
+    {Image: waste, text:"Affordable Waste Solutions", link:"", readMore:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+    {Image: waste2, text:"eco-friendly commerce", link:"", readMore:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+    {Image:  waste, text:"Community Cleanups", link:"", readMore:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+    {Image: waste2, text:"Community Education", link:"", readMore:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+    {Image: partnership, text:"Strategic Partnerships", link:"", readMore:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+    {Image: partnership, text:"Spreading Faith & Hope", link:"", readMore:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
     
 ]
 
 const WhatWeDo = () => {
     return (
         <div>
-            <div className="bg-[#546a45]">
+            <div className="bg-[#184E35]">
                 <NavBar />
             </div>
             
             <div className="px-20 py-30 flex flex-col items-center justify-center">
-                 <h1 className="lg:text-[64px] text-black text-center pb-15 font-bold">What we do</h1>
+                 <h1 className="lg:text-[64px] text-[#184E35] text-center pb-15">What we do</h1>
             <p className="lg:text-[20px] max-w-270 pb-10">At The Unsolicited Chosen, we are committed to restoring dignity to our communities through environmental action. Our work includes organizing regular community cleanups, offering affordable waste management solutions for underserved areas, forming strategic partnerships with local and national stakeholders, educating residents on best cleanliness practices, and initiating tree-planting campaigns to green our neighborhoods. </p>
             <p className="lg:text-[20px] max-w-270 pb-25">As a faith-inspired initiative, we also believe in nurturing the spirit spreading hope and encouragement through faith-based messages and community fellowship as we serve together.</p>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 mb-20">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mb-20">
                 {items.map((item, index) => (
-                    <div key={index} className="relative h-130 w-141 bg-cover bg-center bg-no-repeat text-center flex justify-center  items-center text-white"
+                    <div key={index} className="relative h-120 w-141 bg-cover bg-center bg-no-repeat text-center flex justify-center  items-center text-white
+                    group transition duration-300"
                         style={{backgroundImage: `url(${item.Image})`}}>
-                            <div className="absolute inset-0 bg-black/70 "></div>
-                            <p className="relative text-[32px] font-medium">{item.text}</p>
+                            <div className="absolute inset-0 bg-black/70 transition duration-300"></div>
+                            <p className="relative text-[32px] font-medium group-hover:opacity-0">{item.text}</p>
+                            {/* Hover content */}
+                            <div className="bg-[#184E35] absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 slide-up transition duration-300">
+                                <p className="relative text-[32px] font-medium pb-20 transition-transform duration-300 transform translate-y-4 group-hover:translate-y-0">{item.text}</p>
+                                <p className="pb-4 transition-transform duration-300 transform translate-y-4 group-hover:translate-y-0">{item.readMore}</p>
+                                <p className="text-lg font-semibold mb-4 hover:text-[#818F39] transition-transform duration-300 transform translate-y-4 group-hover:translate-y-0">Read more →</p>
+                            
+                            </div>
+
                     </div>
-                ))}
+                ))} 
             </div>
             </div>
             <div className="bg-[#F5FAF6] pb-20 pt-30">
-                  <div className="border border-[#82A718] px-8 py-30 mx-50 text-center">
-                <p className="text-[32px]">We clean, educate, plant, partner and serve. The Unsolicited Chosen is on a mission to make Nairobi and beyond cleaner, greener, and more hopeful through action and faith.</p>
+                  <div className="border border-[#818F39] px-8 py-30 mx-60 text-center">
+                <p className="text-2xl">We clean, educate, plant, partner and serve. The Unsolicited Chosen is on a mission to make Nairobi and beyond cleaner, greener, and more hopeful through action and faith.</p>
             </div>
+             
              </div>
 
           
