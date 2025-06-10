cf96974eac5fdd2658b3115f6988d77d097d69af
Ayiera-20 <ayieramyra@gmail.com>
2025-06-11 00:09:33 +0300

    style: adjust header font-size for better resposiveness

--- src/routes/Home/Hero.tsx
+++ src/routes/Home/Hero.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
             
             <div className="items-center relative flex flex-col justify-center h-full text-white text-center">
                 <p className="font-bold text-[1.5rem] md:text-[1.75rem] lg:text-[2rem]">Welcome</p>
-                <h1 className="text-[2.5rem] md:text-[3.125rem] lg:text-[4rem] text-[#F5F1E3]">Cleaning Nairobi. Restoring Dignity. <br></br>Inspiring Change.</h1>
+                <h1 className="text-[1.75rem] md:text-[3.125rem] lg:text-[4rem] text-[#F5F1E3]">Cleaning Nairobi. Restoring Dignity. <br></br>Inspiring Change.</h1>
                 <button className="discover-button">Discover</button>
             </div>
 
