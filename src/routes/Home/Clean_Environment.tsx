9c04d63f7a9083578fe4e7b4a5f4bdac3b95a40a
Ayiera-20 <ayieramyra@gmail.com>
2025-06-11 00:10:59 +0300

    style(Home): adjust image sizes and padding for better responsiveness

--- src/routes/Home/Clean_Environment.tsx
+++ src/routes/Home/Clean_Environment.tsx
@@ -24,14 +24,14 @@ const CleanEnvironment = () => {
             <div className="flex flex-col lg:flex-row lg:gap-50 md:gap-30 justify-center items-center lg:items-start">
                 
                 <div className="flex relative w-[400px] h-[500px] items-center md:items-start justify-center md:justify-items-start">
-                <div className="absolute bg-white lg:w-113 lg:h-182 md:w-100 md:h-100 w-80 h-80  bg-cover bg-center bg-no-repeat"
+                <div className="absolute bg-white lg:w-113 lg:h-182 md:w-100 md:h-100 w-60 h-80  bg-cover bg-center bg-no-repeat"
                         style={{backgroundImage: `url(${vegetation})`}}>
                     </div>
-                    <div className="md:top-70 lg:top-90 top-80 md:-left-40 left-4 bg-white  w-25 h-25 md:w-70 md:h-70  bg-cover bg-center bg-no-repeat  absolute"
+                    <div className="md:top-70 lg:top-90 top-80 md:-left-40 left-10 bg-white  w-25 h-25 md:w-70 md:h-70  bg-cover bg-center bg-no-repeat  absolute"
                         style={{backgroundImage: `url(${children})`}}>
                     </div>
                     
-                    <div className=" bottom-80 md:-right-40 right-2 bg-white md:w-58 md:h-58 w-20 h-20 bg-cover bg-center bg-no-repeat absolute"
+                    <div className=" bottom-80 md:-right-40 right-10 bg-white md:w-58 md:h-58 w-20 h-20 bg-cover bg-center bg-no-repeat absolute"
                         style={{backgroundImage: `url(${garbageCollection})`}}>
                     </div>
                 </div>
--- src/routes/Home/Get_Invloved.tsx
+++ src/routes/Home/Get_Invloved.tsx
@@ -1,7 +1,7 @@
 const GetInvolved = () => {
     return (
-        <div className="bg-[#184E35] border-1 border-b-[#818F39] py-16 justify-center items-center flex">
-             <button className="py-4 px-8 rounded-full bg-[#818F39] text-white shadow-2xl border-transparent hover:bg-transparent border-2 hover:border-[#A3B18A] font-bold" >Get Involved</button>
+        <div className="bg-[#184E35] border-1 border-b-[#818F39] md:py-16 py-10 justify-center items-center flex">
+             <button className="md:py-4 md:px-8 py-2 px-4 rounded-full bg-[#818F39] lg:text-lg md:text-base text-sm text-white shadow-2xl border-transparent hover:bg-transparent border-2 hover:border-[#A3B18A] font-bold" >Get Involved</button>
         </div>
     )
 
