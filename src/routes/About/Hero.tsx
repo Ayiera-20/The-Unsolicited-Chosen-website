1ccb7f8dc2d8a3945de2aaef4fd157a988200f4d
Ayiera-20 <ayieramyra@gmail.com>
2025-07-08 18:49:54 +0300

    style: add slideshow to hero section

--- src/routes/About/Hero.tsx
+++ src/routes/About/Hero.tsx
@@ -1,13 +1,43 @@
-import recyc from "../../assets/images/Intro-background.jpg"
 import NavBar from "../../components/navbar";
+import { useEffect, useState } from "react";
+import SectionTransition from "../../components/transition";
+
+const slides = [
+  "/src/assets/images/beautiful-japanese-forest-landscape.jpg",
+  "/src/assets/images/Intro-background.jpg",
+  "/src/assets/images/hero.jpg"
+];
 
 const Hero = () => {
+    const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % slides.length);
+    }, 5000); 
+
+    return () => clearInterval(interval);
+  }, []);
+
     return(
-        <div> 
-            <div className="relative md:h-screen h-[90vh] w-full bg-cover bg-center bg-no-repeat"
-            style={{backgroundImage: `url(${recyc})`}}
-            >
-            <NavBar />
+        <SectionTransition> 
+            <div className="relative md:h-screen h-[90vh] w-full overflow-hidden">
+                <div className="absolute inset-0 z-0">
+                    {slides.map((image, index) => (
+                    <div
+                        key={index}
+                        className={`absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat transition-opacity duration-1000 ease-in-out 
+                        ${index === current ? "opacity-100" : "opacity-0"}`}
+                        style={{ backgroundImage: `url(${image})` }}
+                    />
+                    ))}
+                </div>
+
+                <div className="relative z-10">
+                    <NavBar />
+
+                </div>
+            
 
             <div className="flex flex-col md:flex-row items-center justify-center backdrop-blur-sm absolute bottom-0 left-0 w-full md:h-[40vh] h-auto   lg:gap-x-20 md:gap-0 gap-2 text-white text-center md:text-left bg-black/30 border-t-2 border-[#799434] pt-10">
                 <div className="md:pr-20">
@@ -32,7 +62,7 @@ const Hero = () => {
                 </div>
             
             </div>
-        </div>
+        </SectionTransition>
     )
 }
 
