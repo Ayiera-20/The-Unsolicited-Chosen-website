1c4a8ba7e746609f93f6d266c4ae2ca5dac56139
Ayiera-20 <ayieramyra@gmail.com>
2025-07-09 14:54:10 +0300

    Fix: Adjust z-index for correct stacking and layout display

--- src/routes/Home/Hero.tsx
+++ src/routes/Home/Hero.tsx
@@ -10,19 +10,19 @@ const HeroSection = () => {
       <div className="relative h-[70vh] lg:h-screen w-full overflow-hidden">
     
       <div
-        className="absolute top-0 left-0 w-full h-full bg-cover bg-center animate-zoom-in"
-        style={{ backgroundImage: `url(${heroImage})`, zIndex: 0 }}
+        className="absolute top-0 left-0 w-full h-full bg-cover bg-center animate-zoom-in z-0"
+        style={{ backgroundImage: `url(${heroImage})`}}
       ></div>
 
 
-      <div className="absolute inset-0 bg-black/50 z-10" />
+      <div className="absolute inset-0 bg-black/50 z-10 pointer-events-none" />
 
 
       <div className="relative z-20">
         <NavBar />
       </div>
 
-      <div className="relative z-20 flex flex-col justify-center items-center h-full text-white text-center px-4">
+      <div className="relative z-10 flex flex-col justify-center items-center h-full text-white text-center px-4">
         <p className="font-bold text-[1.5rem] md:text-[1.75rem] lg:text-[2rem]">Welcome</p>
         <h1 className="text-[1.75rem] md:text-[3.125rem] lg:text-[4rem] text-[#F5F1E3]">
           Cleaning Nairobi. Restoring Dignity. <br /> Inspiring Change.
