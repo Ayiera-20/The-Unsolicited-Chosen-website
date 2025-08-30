2f27c6b97ae3add3adb964afd6d635829039c7d2
Omuse Geoffrey <omuse@Omuses-MacBook-Pro.local>
2025-08-30 19:39:31 +0300

    unsolo

---
+++ src/components/transition.tsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+import type { ReactNode } from "react";
+
+type SectionTransitionProps = {
+  children: ReactNode;
+  bgColor?: string;
+};
+
+const SectionTransition = ({ children, bgColor = "#184E35" }: SectionTransitionProps) => {
+  return (
+     <div className="relative overflow-hidden">
+      <motion.div
+        className="absolute top-0 left-0 w-full h-full z-50 origin-bottom"
+        style={{ backgroundColor: bgColor }}
+        initial={{ scaleY: 1 }}
+        animate={{ scaleY: 0 }}
+        transition={{ duration: 1, delay: 0.6, ease: [0.22, 1, 0.36, 1] }}
+      />
+      <div className="relative z-10">{children}</div>
+    </div>
+  );
+};
+
+export default SectionTransition;
